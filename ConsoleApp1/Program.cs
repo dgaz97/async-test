a508cda1de7f48b79e0b9d7d0bc2a0d0f96fd1e6
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2021-02-22 14:27:15 +0100

    Basic System.Threading.Thread

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
@@ -10,6 +11,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Thread monitorThread = new Thread(new ThreadStart(MonitorNetwork));
+            monitorThread.Start();
+
+            Console.ReadKey();
+        }
+
+        static void MonitorNetwork()
+        {
+            long s = 0;
+            for (int i = 1; i <= 5000000; i++)
+            {
+                s += i;
+                if (i % 10000 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine(s);
         }
     }
 }
